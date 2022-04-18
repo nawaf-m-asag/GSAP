50c1e7f3e37a2761942df6ce088f20c1ddb298e9
Nawaf-m-asag <nm.nawaf2017@gmail.com>
2022-04-18 13:51:00 +0300

    gsap

---
+++ script.js
@@ -0,0 +1,15 @@
+var t1=gsap.timeline({defaults:{duration:.7,opacity:0,ease:Back.easeOut.config(1.7)}})
+var t2=gsap.timeline({defaults:{duration:1.5,delay:1}})
+var t3=gsap.timeline({defaults:{duration:1.5}})
+
+t1.from(".card-bg", {delay:1,scale:.2,transformOrigin:"center"})
+  .from(".card-top",{scaleY:0,transformOrigin:"top"})
+  .from(".icon",{scale:.2,transformOrigin:"center"},"-=.7")
+  .from(".blip1",{scaleX:0})
+  .from(".blip2",{scaleX:0},"-=.2")
+  .from(".blip3",{scaleX:0},"-=.3")
+  .from(".blip4",{scaleX:0},"-=.4")
+  .from(".blip5",{scaleX:0},"-=.")
+
+  t2.to(".whole-card",{y:10,repeat:-1,yoyo:true})
+  t3.from(".main-content",{opacity:0,y:40,stagger:.3})
